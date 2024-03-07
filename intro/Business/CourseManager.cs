f47dfd8bb789a23ddeb5432b4ce0c83b485f3cf8
bunyaminfidan <bunyamnfdan@gmail.com>
2024-03-07 16:15:04 +0000

    End of the first day camp
    We created 2 more class one of Entities ve built  in this class some dabatase object, also we created CourseManager class for take control function

---
+++ intro/Business/CourseManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using intro.Entities;
+
+namespace intro.Business;
+
+public class CourseManager
+
+{
+
+
+    Course[] courses = new Course[2];
+
+
+    public CourseManager()
+	{
+
+        Course course1 = new();
+        Course course2 = new();
+
+        course1.Id = 1;
+        course1.Name = "C# Bootcamp";
+        course1.Description = "You will learn basic C# and algroritm";
+        course1.Price = 25.99;
+
+
+        course2.Id = 2;
+        course2.Name = "Java Bootcamp";
+        course2.Description = "You will learn basic Java and algroritm";
+        course2.Price = 35.99;
+
+        courses[0] = course1;
+        courses[1] = course2;
+    }
+
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+
+
+}
+
--- intro/Program.cs
+++ intro/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using intro.Business;
+using intro.Entities;
+
 Console.WriteLine("Hello there, World!");
 
+CourseManager courseManager = new();
+
+Course[] courses = courseManager.GetAll();
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Id);
+    Console.WriteLine(courses[i].Name);
+    Console.WriteLine(courses[i].Description);
+    Console.WriteLine(courses[i].Price);
+
+    Console.WriteLine("-----------------");
+}
