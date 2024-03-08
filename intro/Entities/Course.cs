a0edc6eabf0f2d25f70c4ae50ea7d572e45dffbc
bunyaminfidan <bunyamnfdan@gmail.com>
2024-03-08 17:19:00 +0000

    edited folder
    edited fodler

---
+++ intro/Entities/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace intro.Entities;
+
+public class Course
+{
+	public Course()
+	{
+	}
+
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public double Price { get; set; }
+}
+
